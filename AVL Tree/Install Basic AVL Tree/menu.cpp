d8b3d3a316b4fbc13a180653637baf328531b7da
Lam Tan Trong <116331097+lamtantrong@users.noreply.github.com>
2022-11-29 11:01:54 +0700

    Create menu.cpp

---
+++ AVL Tree/Install Basic AVL Tree/menu.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void menu()
+{
+	printf("0. Thoat chuong trinh\n");
+	printf("1. Tao AVL Tree (tu File)\n");
+	printf("2. Hien thi AVL Tree\n");
+	printf("3. Xoa phan tu co gia tri X\n");
+	printf("4. Them phan tu co gia tri X\n");
+}
