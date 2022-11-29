c362617c1dbcd696fa31ca1805a5c15fef2e9b26
Lam Tan Trong <116331097+lamtantrong@users.noreply.github.com>
2022-11-29 11:25:36 +0700

    Create Search_SingleLinkedList.cpp

---
+++ Single Linked List/Search_SingleLinkedList.cpp
@@ -0,0 +1,137 @@
+//Danh Sách Liên Kết Đơn (Số Nguyên)
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<time.h>
+struct Node
+{
+	int info;
+	Node* next;
+};
+struct SList
+{
+	Node* head;
+	Node* tail;
+};
+void initSList(SList& list);
+int isEmpty(SList list);
+Node* createNode(int x);
+void insertLast(SList& list, Node* p);
+Node* findNode(SList list, int x);
+void nhapTuDong(SList& list, int& soPhanTu);
+void nhapThuCong(SList& list, int& soPhanTu);
+void printList(SList list);
+int main()
+{
+	//Khai báo danh sách liên kết
+	SList list;
+	int soPhanTu, value, x;
+
+	nhapTuDong(list, soPhanTu);
+	//nhapThuCong(list, soPhanTu);
+	printf("Danh sach vua nhap: ");
+	printList(list);
+
+	printf("\nNhap gia tri can tim: ");
+	scanf_s("%d", &value);
+
+	Node* pf = new Node;
+	pf = findNode(list, value);
+	if (pf == NULL)
+		printf("\nKhong tim thay phan tu co gia tri %d trong danh sach", value);
+	else
+		printf("\nDa tim thay phan tu co gia tri %d trong danh sach", value);
+
+	_getch();
+	return 0;
+}
+void initSList(SList& list)
+{
+	list.head = NULL;
+	list.tail = NULL;
+}
+int isEmpty(SList list)
+{
+	if (list.head == NULL)
+		return 1;//danh sách rỗng
+	return 0;//danh sách không rỗng
+}
+Node* createNode(int x)
+{
+	Node* p = new Node;
+	if (p == NULL)
+	{
+		printf("\nKhong Du Bo Nho");
+		_getch();
+		return NULL;
+	}
+	p->info = x;
+	p->next = NULL;
+	return p;
+}
+void insertLast(SList& list, Node* p)
+{
+	if (list.head == NULL)
+	{
+		list.head = p;
+		list.tail = p;
+	}
+	else
+	{
+		list.tail->next = p;
+		list.tail = p;
+	}
+}
+Node* findNode(SList list, int x)
+{
+	for (Node* p = list.head; p != NULL; p = p->next)
+		if (p->info == x)
+			return p;
+	return NULL;
+}
+void nhapTuDong(SList& list, int& soPhanTu)
+{
+	do
+	{
+		printf("Nhap so phan tu (n > 0): ");
+		scanf_s("%d", &soPhanTu);
+	} while (soPhanTu <= 0);
+
+	initSList(list);
+
+	srand(time(NULL));
+	for (int i = 0; i < soPhanTu; i++)
+	{
+		int x = 1 + rand() % 100;
+		Node* p = new Node;
+		p = createNode(x);
+		if (p)//tạo thành công
+			insertLast(list, p);
+	}
+}
+void nhapThuCong(SList& list, int& soPhanTu)
+{
+	do
+	{
+		printf("Nhap so phan tu (n > 0): ");
+		scanf_s("%d", &soPhanTu);
+	} while (soPhanTu <= 0);
+
+	initSList(list);
+
+	for (int i = 1; i <= soPhanTu; i++)
+	{
+		int x;
+		printf("Nhap phan tu thu %d: ", i);
+		scanf_s("%d", &x);
+		Node* p = new Node;
+		p = createNode(x);
+		if (p)//tạo thành công
+			insertLast(list, p);
+	}
+}
+void printList(SList list)
+{
+	for (Node* p = list.head; p != NULL; p = p->next)
+		printf("%5d", p->info);
+}
