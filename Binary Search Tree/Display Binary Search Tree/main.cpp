51b9a48c4d95ae179d8f88e23ef7d3737c2ab1c9
Lam Tan Trong <116331097+lamtantrong@users.noreply.github.com>
2022-11-29 11:07:24 +0700

    Create main.cpp

---
+++ Binary Search Tree/Display Binary Search Tree/main.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include"asciinode.h"
+#include"BinarySearchTree.h"
+
+int main()
+{
+    //Nơi thực thi các hàm và lệnh đã gọi
+    //Khai báo cục bộ trong hàm
+    //Các câu lệnh dùng để định nghĩa hàm main
+    //Dùng lệnh để hiển thị cây tìm kiếm nhị phân: print_ascii_tree
+    return 0;
+}
